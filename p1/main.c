c5fe02e58f02d5c911fc1177f23057b78ecebc69
2025050047 <amudomolla3@gmail.com>
2026-04-15 15:59:27 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-    int answer = 0, input = 0, try = 0;
+    int answer = 0, input = 0, try1 = 0;
     scanf("%d", &answer);
     do {
-        try++;
+        try1++;
         
         scanf("%d", &input);
         
         if (answer > input) printf("%d<?\n", input);
         else if (answer < input) printf("%d>?\n", input);
-        else printf("%d==?\n%d", input, try);
+        else printf("%d==?\n%d", input, try1);
         
     } while (answer != input);
 
